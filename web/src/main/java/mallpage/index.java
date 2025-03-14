d1c61cd6f00d8dc6ab51076374812fbae7e6363e
mekeyace2 <mekeyace2@gmail.com>
2025-03-14 09:18:42 +0900

    dfhdfh

--- web/src/main/java/mallpage/index.java
+++ web/src/main/java/mallpage/index.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package mallpage;
 
 import java.io.IOException;
@@ -29,49 +28,3 @@ public class index extends HttpServlet {
 	}
 
 }
-
-
-
-
-
-
-
-=======
-package mallpage;
-
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/mallpage/index.do")
-public class index extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-    //copyright 정보 Model
-	copyright cr = new copyright();
-	
-
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ArrayList<String> cpdata = this.cr.copyright_info();
-		request.setAttribute("cpdata", cpdata);
-			
-		RequestDispatcher rd = request.getRequestDispatcher("./index.jsp");
-		rd.forward(request, response);
-	}
-
-}
-
-
-
-
-
-
-
->>>>>>> branch 'main' of https://github.com/mekeyace2/web_servlet.git
